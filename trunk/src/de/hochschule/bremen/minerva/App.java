a01935dd9ef5ed08d1e0bcdb05e63b1bebcf0cbe
andre.koenig@gmail.com <andre.koenig@gmail.com@d8c40a07-8e69-ed8f-530b-57c808529c20>
2010-03-30 15:07:39 +0000

    Ich habe eine neue Ausgabezeile hinzugefügt.

--- trunk/src/de/hochschule/bremen/minerva/App.java
+++ trunk/src/de/hochschule/bremen/minerva/App.java
@@ -36,6 +36,7 @@ public class App {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hallo Welt");
+		System.out.println("André hat das Projekt erfolgreich ausgecheckt :D");
 	}
 
 }
