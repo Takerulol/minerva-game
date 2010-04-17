fee9b8a66f182dc84d37d3b00fdb0dceb754afae
andre.koenig <andre.koenig@d8c40a07-8e69-ed8f-530b-57c808529c20>
2010-04-17 09:53:22 +0000

    Added svn keyword.

--- trunk/src/de/hochschule/bremen/minerva/persistence/db/exceptions/DatabaseIOException.java
+++ trunk/src/de/hochschule/bremen/minerva/persistence/db/exceptions/DatabaseIOException.java
@@ -2,7 +2,7 @@
  * Minerva - Game, Copyright 2010 Christian Bollmann, Carina Strempel, André König
  * Hochschule Bremen - University of Applied Sciences - All Rights Reserved.
  *
- * $Id: WorldHandler.java 44 2010-04-07 10:20:55Z andre.koenig $
+ * $Id$
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
