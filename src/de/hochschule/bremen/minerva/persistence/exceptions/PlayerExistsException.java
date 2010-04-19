377407247ae63d3d3998ee92f110dfe263b76fce
cbollmann@stud.hs-bremen.de <cbollmann@stud.hs-bremen.de@d8c40a07-8e69-ed8f-530b-57c808529c20>
2010-04-19 10:02:54 +0000

    minor fix.

--- src/de/hochschule/bremen/minerva/persistence/exceptions/PlayerExistsException.java
+++ src/de/hochschule/bremen/minerva/persistence/exceptions/PlayerExistsException.java
@@ -1,3 +1,33 @@
+/*
+ * Minerva - Game, Copyright 2010 Christian Bollmann, Carina Strempel, André König
+ * Hochschule Bremen - University of Applied Sciences - All Rights Reserved.
+ *
+ * $Id: PersistenceIOException.java 76 2010-04-17 17:04:20Z andre.koenig $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Contact:
+ *     Christian Bollmann: cbollmann@stud.hs-bremen.de
+ *     Carina Strempel: cstrempel@stud.hs-bremen.de
+ *     André König: akoenig@stud.hs-bremen.de
+ * 
+ * Web:
+ *     http://minerva.idira.de
+ * 
+ */
+
 package de.hochschule.bremen.minerva.persistence.exceptions;
 
 public class PlayerExistsException extends PersistenceIOException {
