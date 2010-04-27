726e0ace8434b8a6143903bf141d67f87f074803
Carina.Strempel <Carina.Strempel@d8c40a07-8e69-ed8f-530b-57c808529c20>
2010-04-27 14:12:14 +0000

    First cui draft.

--- trunk/src/de/hochschule/bremen/minerva/ui/cui/MinervaCUI.java
+++ trunk/src/de/hochschule/bremen/minerva/ui/cui/MinervaCUI.java
@@ -33,19 +33,87 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import de.hochschule.bremen.minerva.core.Game;
+import de.hochschule.bremen.minerva.core.Turn;
+import de.hochschule.bremen.minerva.ui.cui.exceptions.CuiException;
+import de.hochschule.bremen.minerva.ui.cui.exceptions.InvalidInputFormatException;
+
 public class MinervaCUI {
 
+	private Game game = null;
 	private BufferedReader console = null;
 	
+	/**
+	 * DOCME
+	 * 
+	 */
 	public MinervaCUI() {
 		this.console = new BufferedReader(new InputStreamReader(System.in));
+	}
+
+	/**
+	 * DOCME
+	 * 
+	 */
+	public void run() {
+		this.gameInit();
+		
+		do {
+			Turn turn = game.nextTurn();
+			
+		} while (!game.isFinished());
+	}
+
+	private void gameInit() {
+		this.out("Hallo Welt");
+		this.out("Bitte geben Sie die Anzahl der Spieler an: ");
+		
+	}
+
+	/**
+	 * DOCME
+	 * 
+	 * @return
+	 * @throws CuiException 
+	 * @throws InvalidInputFormatException 
+	 */
+	private int readInt() throws CuiException, InvalidInputFormatException {
+		int returnValue = 0;
+		
 		try {
-			String input = this.console.readLine();
-			System.out.println(input);
+			String line = this.read();
+			returnValue = Integer.parseInt(line);
+		} catch (NumberFormatException e) {
+			throw new InvalidInputFormatException();
+		} catch (CuiException e) {
+			throw new CuiException("Error while reading from console.");
+		}
+		
+		return returnValue;
+	}
+
+	/**
+	 * DOCME
+	 * 
+	 * @return
+	 * @throws CuiException
+	 */
+	private String read() throws CuiException {
+		try {
+			return this.console.readLine();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			throw new CuiException();
 		}
 	}
+	
+	/**
+	 * DOCME
+	 * 
+	 * @param message
+	 */
+	private void out(String message) {
+		System.out.print(message);
+	}
+
 	
 }
