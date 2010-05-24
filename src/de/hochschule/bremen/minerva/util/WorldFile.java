bdd451b2f62a7e45605a711647ea0df8ec2236ee
andre.koenig <andre.koenig@d8c40a07-8e69-ed8f-530b-57c808529c20>
2010-05-24 16:52:05 +0000

    Added new structure.

--- src/de/hochschule/bremen/minerva/util/WorldFile.java
+++ src/de/hochschule/bremen/minerva/util/WorldFile.java
@@ -41,6 +41,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
@@ -54,6 +55,14 @@ import de.hochschule.bremen.minerva.vo.World;
 public class WorldFile extends World {
 	
 	private static final String MESSAGE_FILE_NOT_WELLFORMED = "The world import file is not well-formed. ";
+
+	// The xml tag sections
+	private static final String TAG_META = "meta";
+
+	private static final String TAG_CONTINENTS = "continents";
+
+	private static final String TAG_COUNTRIES = "countries";
+	
 	
 	private File worldFile = null;
 
@@ -92,45 +101,10 @@ public class WorldFile extends World {
 		try {
 			Element root = this.open();
 
-			// The worlds token
-			this.setToken(this.extractText(root, "token"));
-			if (this.getToken().isEmpty()) {
-				throw new WorldFileParseException(MESSAGE_FILE_NOT_WELLFORMED + "Missing 'token'.");
-			}
-
-			// The worlds name
-			this.setName(this.extractText(root, "name"));
-			if (this.getName().isEmpty()) {
-				throw new WorldFileParseException(MESSAGE_FILE_NOT_WELLFORMED + "Missing 'name'.");
-			}
-
-			// The worlds description
-			this.setDescription(this.extractText(root, "description"));
-			if (this.getDescription().isEmpty()) {
-				throw new WorldFileParseException(MESSAGE_FILE_NOT_WELLFORMED + "Missing 'description'.");
-			}
-			
-			// The author
-			this.setAuthor(this.extractText(root, "author"));
-			if (this.getAuthor().isEmpty()) {
-				throw new WorldFileParseException(MESSAGE_FILE_NOT_WELLFORMED + "Missing 'author'.");
-			}
-
-			// The version
-			this.setVersion(this.extractText(root, "version"));
-			if (this.getVersion().isEmpty()) {
-				throw new WorldFileParseException(MESSAGE_FILE_NOT_WELLFORMED + "Missing 'version'.");
-			}
-
-			// Extract the continents
-			this.extractContinents(root, "continent");
-			if (this.temporallyContinents.isEmpty()) {
-				throw new WorldFileParseException(MESSAGE_FILE_NOT_WELLFORMED + "Missing 'continents'.");
-			}
-
-			// Extract the countries
-			this.extractCountries(root, "country");
-
+			this.validate(root);
+			this.extractMeta(root);
+			this.extractContinents(root);
+			this.extractCountries(root);
 		} catch (IOException e) {
 			throw new WorldFileNotFoundException(e.getMessage());
 		} catch (SAXException e) {
@@ -140,6 +114,14 @@ public class WorldFile extends World {
 		}
 	}
 
+	/**
+	 * DOCME
+	 * 
+	 */
+	public void createCountryDependencies() {
+		
+	}
+	
 	/**
 	 * Starts the parser and returns the document root.
 	 * 
@@ -167,12 +149,53 @@ public class WorldFile extends World {
 	}
 
 	/**
-	 * DOCME
+	 * Extracts the meta data from the world import file
+	 * and pushs the data into the object attributes.
 	 * 
-	 * @param root
+	 * @param root - The document root @see {@link WorldFile#open()}
+	 * @throws WorldFileParseException - If the document is not well-formed.
 	 */
-	private void extractContinents(Element root, String tag) {
-		NodeList continents = root.getElementsByTagName(tag);
+	private void extractMeta(Element root) throws WorldFileParseException {
+		// The worlds token
+		this.setToken(this.extractText(root, "token"));
+		if (this.getToken().isEmpty()) {
+			throw new WorldFileParseException(MESSAGE_FILE_NOT_WELLFORMED + "Missing 'token'.");
+		}
+
+		// The worlds name
+		this.setName(this.extractText(root, "name"));
+		if (this.getName().isEmpty()) {
+			throw new WorldFileParseException(MESSAGE_FILE_NOT_WELLFORMED + "Missing 'name'.");
+		}
+
+		// The worlds description
+		this.setDescription(this.extractText(root, "description"));
+		if (this.getDescription().isEmpty()) {
+			throw new WorldFileParseException(MESSAGE_FILE_NOT_WELLFORMED + "Missing 'description'.");
+		}
+		
+		// The author
+		this.setAuthor(this.extractText(root, "author"));
+		if (this.getAuthor().isEmpty()) {
+			throw new WorldFileParseException(MESSAGE_FILE_NOT_WELLFORMED + "Missing 'author'.");
+		}
+
+		// The version
+		this.setVersion(this.extractText(root, "version"));
+		if (this.getVersion().isEmpty()) {
+			throw new WorldFileParseException(MESSAGE_FILE_NOT_WELLFORMED + "Missing 'version'.");
+		}
+	}
+	
+	/**
+	 * Extracts the continent data from the world import file
+	 * and saves the data in an temporally hash map.
+	 * 
+	 * @param root - The document root @see {@link WorldFile#open()}
+	 * @throws WorldFileParseException - If the document is not well-formed.
+	 */
+	private void extractContinents(Element root) throws WorldFileParseException {
+		NodeList continents = root.getElementsByTagName("continent");
 		
 		for (int i = 0; i < continents.getLength(); i++) {
 			NamedNodeMap node = continents.item(i).getAttributes();
@@ -181,6 +204,10 @@ public class WorldFile extends World {
 
 			int id = Integer.parseInt(node.getNamedItem("id").getNodeValue());
 			this.temporallyContinents.put(id, continent);
+		}
+		
+		if (this.temporallyContinents.isEmpty()) {
+			throw new WorldFileParseException(MESSAGE_FILE_NOT_WELLFORMED + "Missing 'continents' data.");
 		}
 	}
 
@@ -191,9 +218,10 @@ public class WorldFile extends World {
 	 * @param tag
 	 * TODO: Add well-formed checks
 	 * TODO: Get the color from the file
+	 * @throws WorldFileParseException 
 	 */
-	public void extractCountries(Element root, String tag) {
-		NodeList countries = root.getElementsByTagName(tag);
+	private void extractCountries(Element root) throws WorldFileParseException {
+		NodeList countries = root.getElementsByTagName("country");
 		
 		for (int i = 0; i < countries.getLength(); i++) {
 			NamedNodeMap node = countries.item(i).getAttributes();
@@ -210,14 +238,32 @@ public class WorldFile extends World {
 			int id = Integer.parseInt(node.getNamedItem("id").getNodeValue());
 			this.temporallyCountries.put(id, country);
 		}
+
+		if (this.temporallyCountries.isEmpty()) {
+			throw new WorldFileParseException(MESSAGE_FILE_NOT_WELLFORMED + "Missing 'country' data.");
+		}
 	}
 	
 	/**
 	 * DOCME
+	 * @throws WorldFileParseException 
 	 * 
 	 */
-	public void close() {
+	public void validate(Element root) throws WorldFileParseException {
+		Node node = root.getElementsByTagName(TAG_META).item(0);
+		if (node == null || node.getChildNodes().getLength() <= 0) {
+			throw new WorldFileParseException(MESSAGE_FILE_NOT_WELLFORMED + "Missing 'meta' section.");
+		}
 		
+		node = root.getElementsByTagName(TAG_CONTINENTS).item(0);
+		if (node == null || node.getChildNodes().getLength() <= 0) {
+			throw new WorldFileParseException(MESSAGE_FILE_NOT_WELLFORMED + "Missing 'continents' section.");
+		}
+
+		node = root.getElementsByTagName(TAG_COUNTRIES).item(0);
+		if (node == null || node.getChildNodes().getLength() <= 0) {
+			throw new WorldFileParseException(MESSAGE_FILE_NOT_WELLFORMED + "Missing 'countries' section.");
+		}
 	}
 
 	/**
