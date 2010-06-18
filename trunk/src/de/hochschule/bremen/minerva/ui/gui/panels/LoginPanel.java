d7e180e2ae5feabebd5e1fcbc7afff3a9cccb595
cbollmann@stud.hs-bremen.de <cbollmann@stud.hs-bremen.de@d8c40a07-8e69-ed8f-530b-57c808529c20>
2010-06-18 18:53:14 +0000

    added actually working text fields and button to login panel

--- trunk/src/de/hochschule/bremen/minerva/ui/gui/panels/LoginPanel.java
+++ trunk/src/de/hochschule/bremen/minerva/ui/gui/panels/LoginPanel.java
@@ -30,8 +30,17 @@
 package de.hochschule.bremen.minerva.ui.gui.panels;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Point;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import de.hochschule.bremen.minerva.ui.gui.MinervaGUI;
 import de.hochschule.bremen.minerva.ui.gui.controls.MPasswordField;
@@ -40,7 +49,7 @@ import de.hochschule.bremen.minerva.ui.gui.controls.MTextField;
 public class LoginPanel extends JLayeredPane {
 	
 	private Background background;
-	private MTextField username;
+	private JPanel username;
 	private MPasswordField password;
 	
 	/**
@@ -49,19 +58,59 @@ public class LoginPanel extends JLayeredPane {
 	private static final long serialVersionUID = -6914010423171152967L;
 
 	public LoginPanel() {
-		super();
-		this.setPreferredSize(MinervaGUI.WINDOW_SIZE);
-		this.setLayout(new BorderLayout());
-		this.background = new Background(this.getClass());
-		this.add(this.background,BorderLayout.NORTH,DEFAULT_LAYER);
-		
-//		this.username = new MTextField();
+//		super();
+//		this.setPreferredSize(MinervaGUI.WINDOW_SIZE);
+//		this.setLayout(new BorderLayout());
+//		this.background = new Background(this.getClass());
+//		this.add(this.background,BorderLayout.NORTH,DEFAULT_LAYER);
+//		System.out.println(DEFAULT_LAYER);
+//		
+//		this.username = new JPanel();
+//		this.username.setLayout(new BorderLayout());
+//		MTextField mt = new MTextField();
+//		mt.setBounds(0,0,200,50);
+//		this.username.add(mt, BorderLayout.NORTH);
 //		this.password = new MPasswordField();
 //		
-//		this.username.setLocation(500,500);
-//		this.password.setLocation(500,600);
+////		this.username.setLocation(500,500);
+////		this.password.setLocation(500,600);
 //		
-//		this.add(this.username,BorderLayout.NORTH,PALETTE_LAYER);
-//		this.add(this.password,BorderLayout.NORTH,PALETTE_LAYER);
+//		this.username.setBounds(500, 500, this.username.getWidth(), this.username.getHeight());
+//		this.username.setPreferredSize(new Dimension(500,500));
+//		
+//		this.add(this.username,BorderLayout.NORTH,1);
+//		//this.add(this.password,BorderLayout.NORTH,2);
+		this.setPreferredSize(new Dimension(1000,700));
+		this.setOpaque(true);
+	
+		Background bg = new Background(this.getClass());
+		bg.setBounds(0, 0, 500, 500);
+		
+		
+		MTextField mt = new MTextField();
+		//mt.setPreferredSize(new Dimension(200,50));
+		mt.setBounds(614, 212, 244, 25);
+		mt.setOpaque(true);
+		mt.setVisible(true);
+		
+		MPasswordField mp = new MPasswordField();
+		//mp.setPreferredSize(new Dimension(200,50));
+		mp.setBounds(614, 281, 244, 25);
+		mp.setOpaque(true);
+		mp.setVisible(true);
+		
+		JButton login = new JButton("Let's go!");
+		login.setBounds(793, 328, 65, 26);
+		//login.setFont(new Font("Arial",Font.BOLD,9));
+		
+		
+		this.add(login,20);
+		
+		
+		
+		this.add(mt,20);
+		this.add(mp,20);
+		this.add(bg,10);
+		this.updateUI();
 	}
 }
