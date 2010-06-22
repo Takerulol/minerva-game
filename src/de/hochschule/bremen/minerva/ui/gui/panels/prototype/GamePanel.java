cd9b412b8ebe1847b67c3ae25948e78a64ef7c30
cbollmann@stud.hs-bremen.de <cbollmann@stud.hs-bremen.de@d8c40a07-8e69-ed8f-530b-57c808529c20>
2010-06-22 21:04:16 +0000

    country recognize test

---
+++ src/de/hochschule/bremen/minerva/ui/gui/panels/prototype/GamePanel.java
@@ -0,0 +1,112 @@
+/*
+ * Minerva - Game, Copyright 2010 Christian Bollmann, Carina Strempel, André König
+ * Hochschule Bremen - University of Applied Sciences - All Rights Reserved.
+ *
+ * $Id$
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Contact:
+ *     Christian Bollmann: cbollmann@stud.hs-bremen.de
+ *     Carina Strempel: cstrempel@stud.hs-bremen.de
+ *     André König: akoenig@stud.hs-bremen.de
+ * 
+ * Web:
+ *     http://minerva.idira.de
+ * 
+ */
+
+package de.hochschule.bremen.minerva.ui.gui.panels.prototype;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.event.MouseEvent;
+import java.util.Vector;
+
+import javax.swing.JLayeredPane;
+
+import de.hochschule.bremen.minerva.exceptions.WorldDoesNotExistException;
+import de.hochschule.bremen.minerva.manager.ApplicationConfigurationManager;
+import de.hochschule.bremen.minerva.manager.WorldManager;
+import de.hochschule.bremen.minerva.persistence.exceptions.DataAccessException;
+import de.hochschule.bremen.minerva.ui.gui.MinervaGUI;
+import de.hochschule.bremen.minerva.ui.gui.listener.MMouseListener;
+import de.hochschule.bremen.minerva.vo.Country;
+import de.hochschule.bremen.minerva.vo.World;
+
+/**
+ * Prototype of the actual game screen.
+ * 
+ * @version 
+ * @since
+ *
+ */
+public class GamePanel extends JLayeredPane {
+	public MapPanel lowerMap;
+	public World world;
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2906065533734117968L;
+	
+	public GamePanel() {
+		this.setPreferredSize(MinervaGUI.WINDOW_SIZE);
+		this.setOpaque(true);
+		
+		
+		this.setLayout(new BorderLayout());
+		
+		Vector<World> worlds;
+		try {
+			worlds = WorldManager.getInstance().getList(true);
+			try {
+				world = WorldManager.getInstance().get(worlds.get(2));
+			} catch (WorldDoesNotExistException e1) {
+			} catch (DataAccessException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		} catch (DataAccessException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		String filepath = ApplicationConfigurationManager.get().getWorldsAssetsDirectory() + world.getMapUnderlay();
+		//System.out.println(filepath);
+		
+		//String filepath = "resources/importer/original/b44a59383b3123a747d139bd0e71d2df.jpg";
+		lowerMap = new MapPanel(filepath);
+		this.add(lowerMap,BorderLayout.NORTH,0);
+		
+		lowerMap.addMouseListener(new MMouseListener() {
+			public void mouseClicked(MouseEvent e) {
+				int farbe = GamePanel.this.lowerMap.getMapImage().getRGB(e.getX(), e.getY());
+				String hex = Integer.toHexString(farbe);
+				Color color = new Color(farbe);
+				System.out.println(color);
+				Country country = world.getCountry(color);
+				System.out.println("Farbe: "+hex+" "+country);
+			}
+		});
+		
+		
+		
+		
+		
+		this.updateUI();
+	}
+
+}
