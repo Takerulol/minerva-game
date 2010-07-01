8c67a3c8be77282085ecfbfa836c8b212ce452b1
cbollmann@stud.hs-bremen.de <cbollmann@stud.hs-bremen.de@d8c40a07-8e69-ed8f-530b-57c808529c20>
2010-07-01 14:06:37 +0000

    Fixed lots of beauty problems with control bar. also implemented list for country cards.

--- trunk/src/de/hochschule/bremen/minerva/ui/gui/panels/prototype/ControlBarPanel.java
+++ trunk/src/de/hochschule/bremen/minerva/ui/gui/panels/prototype/ControlBarPanel.java
@@ -31,21 +31,35 @@
 package de.hochschule.bremen.minerva.ui.gui.panels.prototype;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Vector;
 
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
+import javax.swing.DefaultListSelectionModel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
+import javax.swing.ListSelectionModel;
 
+import de.hochschule.bremen.minerva.vo.CavalerieCard;
+import de.hochschule.bremen.minerva.vo.CountryCard;
 import de.hochschule.bremen.minerva.vo.Mission;
 import de.hochschule.bremen.minerva.vo.Player;
+import de.hochschule.bremen.minerva.vo.SoldierCard;
 
 /**
- * Panel sliced
+ * Panel sliced in halves.
+ * The upper half contains current player, allocatable army count, game state buttons
+ * (allocate, turn cards in, attack, move, end turn) and a slide button.
+ * 
+ * The lower half contains game informations like missions, country cards ...
  *
  * @version $Id$
  * @since 1.0
@@ -66,6 +80,9 @@ public class ControlBarPanel extends JPanel implements ActionListener {
 	
 	//lower half
 	private JTextArea MissionText;
+	private JList cardList;
+	private DefaultListModel model;
+	private JButton turnIn;
 	
 	//slide
 	private JButton slideButton;
@@ -80,10 +97,10 @@ public class ControlBarPanel extends JPanel implements ActionListener {
 	private static final long serialVersionUID = 2420450895831821731L;
 	
 	/**
-	 * 
+	 * Constructor
 	 */
 	public ControlBarPanel() {
-		init();
+		this.init();
 	}
 	
 	/**
@@ -91,12 +108,29 @@ public class ControlBarPanel extends JPanel implements ActionListener {
 	 */
 	private void init() {
 		this.setLayout(new BorderLayout());
-		this.setSize(200,30);
+		
+		this.initUpperHalf();
+		this.initLowerHalf();
+		
+		this.add(this.upperHalf, BorderLayout.NORTH);
+		this.add(this.lowerHalf, BorderLayout.SOUTH);
+		
+		this.setPreferredSize(new Dimension(1000,(this.upperHalf.getPreferredSize().height + this.lowerHalf.getPreferredSize().height)));
+		
+		this.updateUI();
+	}
+
+	/**
+	 * Initialization of the UPPER half of the control bar
+	 */
+	private void initUpperHalf() {
 		//upper half
 		this.upperHalf = new JPanel();
+		this.upperHalf.setBackground(Color.LIGHT_GRAY);
 		this.upperHalf.setLayout(new FlowLayout(FlowLayout.CENTER,0,0));
 		
 		this.currentPlayerLabel = new JLabel("CURRENT PLAYER");
+		this.currentPlayerLabel.setPreferredSize(new Dimension(100,26));
 		
 		this.allocateButton = new JButton("Armeen setzen");
 		this.allocatableArmies = new JLabel("0");
@@ -113,27 +147,54 @@ public class ControlBarPanel extends JPanel implements ActionListener {
 		this.upperHalf.add(this.attackButton);
 		this.upperHalf.add(this.moveButton);
 		this.upperHalf.add(this.endTurnButton);
-		
-		
+	}
+
+	/**
+	 * Initialization of the LOWER half of the control bar
+	 */
+	private void initLowerHalf() {
 		//lower half
 		this.lowerHalf = new JPanel();
+		this.lowerHalf.setBackground(Color.LIGHT_GRAY);
 		this.lowerHalf.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
 		
+		//constructing all objects
 		this.MissionText = new JTextArea();
+		this.MissionText.setLineWrap(true);
+		this.MissionText.setWrapStyleWord(true);
+		this.model = new DefaultListModel();
+		this.cardList = new JList(this.model);
+		this.turnIn = new JButton("Turn in!");
+		
+		//mission text
 		this.MissionText.setText("Mission:");
 		this.MissionText.setEditable(false);
-		this.MissionText.setPreferredSize(new Dimension(200,50));
+		this.MissionText.setPreferredSize(new Dimension(150,125));
+		this.MissionText.setBackground(Color.LIGHT_GRAY);
+		this.MissionText.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.CYAN),"Mission"));
+		
+		//card list
+		this.cardList.setVisibleRowCount(5);
+		DefaultListSelectionModel selection = new DefaultListSelectionModel();
+		selection.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		this.cardList.setSelectionModel(selection);
+		this.cardList.setPreferredSize(new Dimension(250,125));
+		this.cardList.setBackground(Color.LIGHT_GRAY);
+		this.cardList.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(Color.CYAN),"Cards"));
+		
+		
 		
 		this.lowerHalf.add(this.MissionText);
-		
-		
-		
-		this.add(this.upperHalf, BorderLayout.NORTH);
-		this.add(this.lowerHalf, BorderLayout.SOUTH);
-		
-		this.updateUI();
+		this.lowerHalf.add(this.cardList);
+		this.lowerHalf.add(this.turnIn);
 	}
 	
+	/**
+	 * Sets slide panel if this panel is inside one. This method is automatically used
+	 * if the control bar is deployed in a slide panel.
+	 * 
+	 * @param slidePanel the slide panel
+	 */
 	public void setSlidePanel(MSlidePanel slidePanel) {
 		this.slidePanel = slidePanel;
 		this.relativeHeight = (int) lowerHalf.getPreferredSize().getHeight();
@@ -143,7 +204,7 @@ public class ControlBarPanel extends JPanel implements ActionListener {
 	}
 	
 	/**
-	 * Adds action listeners to
+	 * Adds action listeners to the slide button if control bar is inside a slide panel
 	 */
 	private void addSlideListeners() {
 		this.slideButton.addActionListener(new ActionListener() {	
@@ -159,26 +220,56 @@ public class ControlBarPanel extends JPanel implements ActionListener {
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * Gets relative height of the control bar.
+	 * @return relative height
 	 */
 	public int getRelativeHeight() {
 		return this.relativeHeight;	
 	}
 	
+	/**
+	 * sets the label for the current player
+	 * @param currentPlayer name of current player
+	 */
 	public void setCurrentPlayerLabel(Player currentPlayer) {
 		this.currentPlayerLabel.setText(" "+currentPlayer.getUsername()+" ");
 		this.currentPlayerLabel.setForeground(currentPlayer.getColor());
 	}
 	
+	/**
+	 * update the card list of the current player to the given one.
+	 * @param cards card list
+	 */
+	public void updateCardList(Vector<CountryCard> cards) {
+		this.model.clear();
+		for (CountryCard card : cards) {
+			String text;
+			if (card instanceof SoldierCard) {
+				text = "Symbol: Soldier, Land: "+card.getReference().getName();
+			} else if (card instanceof CavalerieCard) {
+				text = "Symbol: Cavalerie, Land: "+card.getReference().getName();
+			} else {
+				text = "Symbol: Canon, Land: "+card.getReference().getName();
+			}
+			model.addElement(text);
+		}
+	}
+	
+	/**
+	 * Updates mission text to the current player's.
+	 */
 	public void updateMissionText() {
 		for (Mission mission : this.gamePanel.getGame().getMissions()) {
 			if (mission.getOwner() == this.gamePanel.getGame().getTurns().lastElement().getCurrentPlayer()) {
-				this.MissionText.setText("Mission: \n" +mission.getDescription());
+				this.MissionText.setText(mission.getDescription());
 			}
 		}
 	}
 	
+	/**
+	 * Adds listeners to all Buttons and sets the game panel needed for their states.
+	 * @param gamePanel game panel
+	 */
 	public void addListeners(GamePanel gamePanel) {
 		this.gamePanel = gamePanel;
 		this.endTurnButton.addActionListener(this);
@@ -188,10 +279,17 @@ public class ControlBarPanel extends JPanel implements ActionListener {
 		this.moveButton.addActionListener(this);
 	}
 	
+	/**
+	 * Sets text of the label for allocatable armies.
+	 * @param allocatableArmies count as string
+	 */
 	public void setAllocatableArmiesLabel(String allocatableArmies) {
 		this.allocatableArmies.setText(" "+allocatableArmies+" ");
 	}
 	
+	/**
+	 * Updates the state of all buttons
+	 */
 	public void updateButtons() {
 		if (this.gamePanel.getGameState() > 0) {
 			this.allocateButton.setEnabled(false);
@@ -203,15 +301,21 @@ public class ControlBarPanel extends JPanel implements ActionListener {
 						this.moveButton.setEnabled(false);
 					}
 				}
+			} else {
+				this.turnIn.setEnabled(true);
 			}
 		} else {
 			this.allocateButton.setEnabled(true);
 			this.cardButton.setEnabled(true);
 			this.attackButton.setEnabled(true);
 			this.moveButton.setEnabled(true);
+			this.turnIn.setEnabled(false);
 		}
 	}
 
+	/**
+	 * Buttons actions of this panel
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		//TODO: implementation
