9dff632d151cf9945ba72767c3278ca4ae13461a
andre.koenig <andre.koenig@d8c40a07-8e69-ed8f-530b-57c808529c20>
2010-07-04 07:04:57 +0000

    Source code documentation.

--- trunk/src/de/hochschule/bremen/minerva/ui/gui/controls/MSlidePanel.java
+++ trunk/src/de/hochschule/bremen/minerva/ui/gui/controls/MSlidePanel.java
@@ -65,8 +65,7 @@ public class MSlidePanel extends JPanel {
 		this.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
 		this.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, new Color(19, 20, 22)));
 		this.setBackground(new Color(255, 255, 255, 30));
-		//this.setOpaque(false);
-		
+
 		controlBar.setSlidePanel(this);
 		this.add(controlBar);
 		
