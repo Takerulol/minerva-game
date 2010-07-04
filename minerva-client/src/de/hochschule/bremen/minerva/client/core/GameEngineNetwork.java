6262813663c0b22dd5c5be9962a1e5a8984fe64d
andre.koenig <andre.koenig@d8c40a07-8e69-ed8f-530b-57c808529c20>
2010-07-04 17:08:40 +0000

    Source code documentation.

--- minerva-client/src/de/hochschule/bremen/minerva/client/core/GameEngineNetwork.java
+++ minerva-client/src/de/hochschule/bremen/minerva/client/core/GameEngineNetwork.java
@@ -1,3 +1,32 @@
+/*
+ * Minerva - Game, Copyright 2010 Christian Bollmann, Carina Strempel, André König
+ * Hochschule Bremen - University of Applied Sciences - All Rights Reserved.
+ *
+ * $Id: ApplicationConfiguration.java 699 2010-07-04 17:07:33Z andre.koenig $
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Contact:
+ *     Christian Bollmann: cbollmann@stud.hs-bremen.de
+ *     Carina Strempel: cstrempel@stud.hs-bremen.de
+ *     André König: akoenig@stud.hs-bremen.de
+ * 
+ * Web:
+ *     http://minerva.idira.de
+ * 
+ */
 package de.hochschule.bremen.minerva.client.core;
 
 import java.io.File;
@@ -28,6 +57,14 @@ import de.hochschule.bremen.minerva.commons.vo.Mission;
 import de.hochschule.bremen.minerva.commons.vo.Player;
 import de.hochschule.bremen.minerva.commons.vo.World;
 
+/**
+ * The network game engine.
+ *
+ * @see ApplicationConfigurationManager
+ * @version $Id: ApplicationConfiguration.java 699 2010-07-04 17:07:33Z andre.koenig $
+ * @since 1.0
+ * 
+ */
 public class GameEngineNetwork implements GameEngine {
 
 	public static GameEngineNetwork getEngine(){return null;}
