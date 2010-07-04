74381fcdfa0ff2c7ee75100f5dd6577dd5aa37f9
andre.koenig <andre.koenig@d8c40a07-8e69-ed8f-530b-57c808529c20>
2010-07-04 09:31:37 +0000

    Source code documentation.

--- trunk/src/de/hochschule/bremen/minerva/ui/gui/panels/subpanels/GamePanelControlbar.java
+++ trunk/src/de/hochschule/bremen/minerva/ui/gui/panels/subpanels/GamePanelControlbar.java
@@ -61,36 +61,38 @@ import de.hochschule.bremen.minerva.vo.PlayerState;
 import de.hochschule.bremen.minerva.vo.SoldierCard;
 
 /**
- * Panel sliced in halves.
- * The upper half contains current player, allocatable army count, game state buttons
- * (allocate, turn cards in, attack, move, end turn) and a slide button.
+ * Panel sliced in halves. The upper half contains current player, allocatable
+ * army count, game state buttons (allocate, turn cards in, attack, move, end
+ * turn) and a slide button.
  * 
  * The lower half contains game informations country cards ...
- *
- * @version $Id$
+ * 
+ * @version $Id: GamePanelControlbar.java 626 2010-07-04 09:12:23Z andre.koenig
+ *          $
  * @since 1.0
- *
+ * 
  */
-public class GamePanelControlbar extends JPanel implements ActionListener, MControl, TextResources {
+public class GamePanelControlbar extends JPanel implements ActionListener,
+		MControl, TextResources {
 
 	private int relativeHeight;
 	private MSlidePanel slidePanel;
 	private GamePanel gamePanel;
-	
-	//upper half
+
+	// upper half
 	private JButton allocateButton;
 	private JLabel allocatableArmies;
 	private JButton cardButton;
 	private JButton attackButton;
 	private JButton moveButton;
 	private JButton finishTurnButton;
-	
-	//lower half
+
+	// lower half
 	private JList cardList;
 	private DefaultListModel model;
 	private JButton buttonTurnIn;
-	
-	//slide
+
+	// slide
 	private JButton slideButton;
 	private JPanel upperHalf;
 	private JPanel lowerHalf;
@@ -98,18 +100,18 @@ public class GamePanelControlbar extends JPanel implements ActionListener, MCont
 	private JPanel currentPlayerArea;
 
 	private static final long serialVersionUID = 2420450895831821731L;
-	
+
 	/**
 	 * Constructor
-	 *
+	 * 
 	 */
 	public GamePanelControlbar() {
 		this.init();
 	}
-	
+
 	/**
 	 * Initializes ControlBarPanel.
-	 *
+	 * 
 	 */
 	private void init() {
 		this.setLayout(new BorderLayout());
@@ -117,39 +119,43 @@ public class GamePanelControlbar extends JPanel implements ActionListener, MCont
 
 		this.initUpperHalf();
 		this.initLowerHalf();
-		
+
 		this.add(this.upperHalf, BorderLayout.NORTH);
 		this.add(this.lowerHalf, BorderLayout.SOUTH);
-		
+
 		this.setPreferredSize(new Dimension(1000, 220));
-		
+
 		this.updateUI();
 	}
 
 	/**
 	 * Initialization of the UPPER half of the control bar
-	 *
+	 * 
 	 */
 	private void initUpperHalf() {
-		//upper half
+		// upper half
 		this.upperHalf = new JPanel();
-		this.upperHalf.setLayout(new FlowLayout(FlowLayout.CENTER,0,0));
+		this.upperHalf.setLayout(new FlowLayout(FlowLayout.CENTER, 0, 0));
 		this.upperHalf.setOpaque(false);
 
 		this.currentPlayerArea = new JPanel();
 		this.currentPlayerArea.setOpaque(false);
 
-		this.allocateButton = new JButton(GAME_PANEL_CONTROLBAR_BUTTON_SET_ARMIES);
+		this.allocateButton = new JButton(
+				GAME_PANEL_CONTROLBAR_BUTTON_SET_ARMIES);
 		this.allocatableArmies = new JLabel("0");
 		this.allocatableArmies.setForeground(new Color(187, 186, 187));
 
-		this.cardButton = new JButton(GAME_PANEL_CONTROLBAR_BUTTON_TURN_CARDS_IN);
+		this.cardButton = new JButton(
+				GAME_PANEL_CONTROLBAR_BUTTON_TURN_CARDS_IN);
 		this.attackButton = new JButton(GAME_PANEL_CONTROLBAR_BUTTON_ATTACK);
-		this.moveButton = new JButton(GAME_PANEL_CONTROLBAR_BUTTON_ALLOCATE_ARMIES);
-		this.finishTurnButton = new JButton(GAME_PANEL_CONTROLBAR_BUTTON_FINISH_TURN);
-		
+		this.moveButton = new JButton(
+				GAME_PANEL_CONTROLBAR_BUTTON_ALLOCATE_ARMIES);
+		this.finishTurnButton = new JButton(
+				GAME_PANEL_CONTROLBAR_BUTTON_FINISH_TURN);
+
 		this.upperHalf.add(this.currentPlayerArea);
-		
+
 		this.upperHalf.add(this.cardButton);
 		this.upperHalf.add(this.allocateButton);
 		this.upperHalf.add(this.allocatableArmies);
@@ -160,41 +166,46 @@ public class GamePanelControlbar extends JPanel implements ActionListener, MCont
 
 	/**
 	 * Initialization of the LOWER half of the control bar
-	 *
+	 * 
 	 */
 	private void initLowerHalf() {
-		//lower half
+		// lower half
 		this.lowerHalf = new JPanel();
-		this.lowerHalf.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
+		this.lowerHalf.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
 		this.lowerHalf.setOpaque(false);
-		
-		//constructing all objects
+
+		// constructing all objects
 		this.model = new DefaultListModel();
 		this.cardList = new JList(this.model);
-		this.buttonTurnIn = new JButton(GAME_PANEL_CONTROLBAR_BUTTON_TURN_CARDS_IN);
-		
-		//card list
+		this.buttonTurnIn = new JButton(
+				GAME_PANEL_CONTROLBAR_BUTTON_TURN_CARDS_IN);
+
+		// card list
 		this.cardList.setVisibleRowCount(5);
 		DefaultListSelectionModel selection = new DefaultListSelectionModel();
-		selection.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		selection
+				.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
 
 		this.cardList.setOpaque(false);
 		this.cardList.setSelectionModel(selection);
-		this.cardList.setPreferredSize(new Dimension(250,125));
-		this.cardList.setBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(new Color(187, 186, 187)), GAME_PANEL_CARDS,0,0,FONT,Color.LIGHT_GRAY));
+		this.cardList.setPreferredSize(new Dimension(250, 125));
+		this.cardList.setBorder(BorderFactory.createTitledBorder(
+				BorderFactory.createLineBorder(new Color(187, 186, 187)),
+				GAME_PANEL_CARDS, 0, 0, FONT, Color.LIGHT_GRAY));
 		this.cardList.setFont(FONT);
 		this.cardList.setForeground(new Color(139, 140, 142));
 
 		this.lowerHalf.add(this.cardList);
 		this.lowerHalf.add(this.buttonTurnIn);
 	}
-	
+
 	/**
-	 * Sets slide panel if this panel is inside one. This method is automatically used
-	 * if the control bar is deployed in a slide panel.
-	 *
-	 * @param slidePanel the slide panel
-	 *
+	 * Sets slide panel if this panel is inside one. This method is
+	 * automatically used if the control bar is deployed in a slide panel.
+	 * 
+	 * @param slidePanel
+	 *            the slide panel
+	 * 
 	 */
 	public void setSlidePanel(MSlidePanel slidePanel) {
 		this.slidePanel = slidePanel;
@@ -204,39 +215,41 @@ public class GamePanelControlbar extends JPanel implements ActionListener, MCont
 		this.upperHalf.add(this.slideButton);
 		this.addSlideListeners();
 	}
-	
+
 	/**
-	 * Adds action listeners to the slide button if control bar is inside a slide panel
-	 *
+	 * Adds action listeners to the slide button if control bar is inside a
+	 * slide panel
+	 * 
 	 */
 	private void addSlideListeners() {
-		this.slideButton.addActionListener(new ActionListener() {	
+		this.slideButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				if (GamePanelControlbar.this.slidePanel.isMinimized()) {
 					GamePanelControlbar.this.slidePanel.slideUp();
 				} else {
 					GamePanelControlbar.this.slidePanel.slideDown();
-				}			
+				}
 			}
 		});
 	}
-	
+
 	/**
 	 * Gets relative height of the control bar.
-	 *
+	 * 
 	 * @return relative height
-	 *
+	 * 
 	 */
 	public int getRelativeHeight() {
-		return this.relativeHeight;	
+		return this.relativeHeight;
 	}
-	
+
 	/**
 	 * Sets the label for the current player
-	 *
-	 * @param currentPlayer name of current player
-	 *
+	 * 
+	 * @param currentPlayer
+	 *            name of current player
+	 * 
 	 */
 	public void setCurrentPlayerLabel(Player currentPlayer) {
 		this.currentPlayerArea.removeAll();
@@ -245,30 +258,36 @@ public class GamePanelControlbar extends JPanel implements ActionListener, MCont
 
 	/**
 	 * Update the card list of the current player to the given one.
-	 *
-	 * @param cards card list
-	 *
+	 * 
+	 * @param cards
+	 *            card list
+	 * 
 	 */
 	public void updateCardList(Vector<CountryCard> cards) {
 		this.model.clear();
 		for (CountryCard card : cards) {
 			String text;
 			if (card instanceof SoldierCard) {
-				text = GAME_PANEL_CONTROLBAR_CARD_SYMBOL_SOLDIER+card.getReference().getName();
+				text = GAME_PANEL_CONTROLBAR_CARD_SYMBOL_SOLDIER
+						+ card.getReference().getName();
 			} else if (card instanceof CavalerieCard) {
-				text = GAME_PANEL_CONTROLBAR_CARD_SYMBOL_CAVALERIE+card.getReference().getName();
+				text = GAME_PANEL_CONTROLBAR_CARD_SYMBOL_CAVALERIE
+						+ card.getReference().getName();
 			} else {
-				text = GAME_PANEL_CONTROLBAR_CARD_SYMBOL_CANON+card.getReference().getName();
+				text = GAME_PANEL_CONTROLBAR_CARD_SYMBOL_CANON
+						+ card.getReference().getName();
 			}
 			model.addElement(text);
 		}
 	}
-	
+
 	/**
-	 * Adds listeners to all Buttons and sets the game panel needed for their states.
-	 *
-	 * @param gamePanel game panel
-	 *
+	 * Adds listeners to all Buttons and sets the game panel needed for their
+	 * states.
+	 * 
+	 * @param gamePanel
+	 *            game panel
+	 * 
 	 */
 	public void addListeners(GamePanel gamePanel) {
 		this.gamePanel = gamePanel;
@@ -279,20 +298,21 @@ public class GamePanelControlbar extends JPanel implements ActionListener, MCont
 		this.moveButton.addActionListener(this);
 		this.buttonTurnIn.addActionListener(this);
 	}
-	
+
 	/**
 	 * Sets text of the label for allocatable armies.
-	 *
-	 * @param allocatableArmies count as string
-	 *
+	 * 
+	 * @param allocatableArmies
+	 *            count as string
+	 * 
 	 */
 	public void setAllocatableArmiesLabel(String allocatableArmies) {
-		this.allocatableArmies.setText(" "+allocatableArmies+" ");
+		this.allocatableArmies.setText(" " + allocatableArmies + " ");
 	}
-	
+
 	/**
 	 * Updates the state of all buttons
-	 *
+	 * 
 	 */
 	public void updateButtons() {
 		if (this.gamePanel.getCurrentPlayer().getState() == PlayerState.RELEASE_CARDS) {
@@ -325,12 +345,12 @@ public class GamePanelControlbar extends JPanel implements ActionListener, MCont
 			this.attackButton.setEnabled(false);
 			this.moveButton.setEnabled(false);
 			this.buttonTurnIn.setEnabled(false);
-		} 
+		}
 	}
 
 	/**
 	 * Buttons actions of this panel
-	 *
+	 * 
 	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -338,10 +358,12 @@ public class GamePanelControlbar extends JPanel implements ActionListener, MCont
 			MinervaGUI.getEngine().finishTurn();
 			this.gamePanel.unmarkAll();
 		} else if (e.getSource() == this.allocateButton) {
-			this.gamePanel.getCurrentPlayer().setState(PlayerState.ALLOCATE_ARMIES);
+			this.gamePanel.getCurrentPlayer().setState(
+					PlayerState.ALLOCATE_ARMIES);
 			this.gamePanel.unmarkAll();
 		} else if (e.getSource() == this.cardButton) {
-			this.gamePanel.getCurrentPlayer().setState(PlayerState.RELEASE_CARDS);
+			this.gamePanel.getCurrentPlayer().setState(
+					PlayerState.RELEASE_CARDS);
 			this.gamePanel.unmarkAll();
 		} else if (e.getSource() == this.attackButton) {
 			this.gamePanel.getCurrentPlayer().setState(PlayerState.ATTACK);
@@ -350,17 +372,21 @@ public class GamePanelControlbar extends JPanel implements ActionListener, MCont
 			this.gamePanel.getCurrentPlayer().setState(PlayerState.MOVE);
 			this.gamePanel.unmarkAll();
 		} else if (e.getSource() == this.buttonTurnIn) {
-		
+
 			if (this.cardList.getSelectedIndices().length == 1) {
-				this.gamePanel.TurnCardIn(this.gamePanel.getCurrentPlayer().getCountryCards().get(this.cardList.getSelectedIndex()));
+				this.gamePanel.TurnCardIn(this.gamePanel.getCurrentPlayer()
+						.getCountryCards()
+						.get(this.cardList.getSelectedIndex()));
 			} else if (this.cardList.getSelectedIndices().length == 3) {
 				Vector<CountryCard> series = new Vector<CountryCard>();
 				for (int i = 0; i < 3; i++) {
-					series.add(this.gamePanel.getCurrentPlayer().getCountryCards().get(this.cardList.getSelectedIndices()[i]));
+					series.add(this.gamePanel.getCurrentPlayer()
+							.getCountryCards()
+							.get(this.cardList.getSelectedIndices()[i]));
 				}
 				this.gamePanel.TurnSeriesIn(series);
 			} else {
-				//TODO: Better exception message!!!!
+				// TODO: Better exception message!!!!
 				MMessageBox.error("Das geht nicht.");
 			}
 		}
