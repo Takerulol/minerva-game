cffa1308dfd364e3270a2777e18aadd17ee1336b
andre.koenig <andre.koenig@d8c40a07-8e69-ed8f-530b-57c808529c20>
2010-07-04 16:43:01 +0000

    Project splitting.

---
+++ minerva-commons/src/de/hochschule/bremen/minerva/commons/core/GameEngine.java
@@ -0,0 +1,117 @@
+/*
+ * Minerva - Game, Copyright 2010 Christian Bollmann, Carina Strempel, André König
+ * Hochschule Bremen - University of Applied Sciences - All Rights Reserved.
+ *
+ * $Id$
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Contact:
+ *     Christian Bollmann: cbollmann@stud.hs-bremen.de
+ *     Carina Strempel: cstrempel@stud.hs-bremen.de
+ *     André König: akoenig@stud.hs-bremen.de
+ * 
+ * Web:
+ *     http://minerva.idira.de
+ * 
+ */
+package de.hochschule.bremen.minerva.commons.core;
+
+import java.io.File;
+import java.util.Vector;
+
+import de.hochschule.bremen.minerva.commons.exceptions.CountriesNotInRelationException;
+import de.hochschule.bremen.minerva.commons.exceptions.CountryOwnerException;
+import de.hochschule.bremen.minerva.commons.exceptions.DataAccessException;
+import de.hochschule.bremen.minerva.commons.exceptions.GameAlreadyStartedException;
+import de.hochschule.bremen.minerva.commons.exceptions.IsOwnCountryException;
+import de.hochschule.bremen.minerva.commons.exceptions.NoPlayerLoggedInException;
+import de.hochschule.bremen.minerva.commons.exceptions.NoPlayerSlotAvailableException;
+import de.hochschule.bremen.minerva.commons.exceptions.NotEnoughArmiesException;
+import de.hochschule.bremen.minerva.commons.exceptions.NotEnoughPlayersLoggedInException;
+import de.hochschule.bremen.minerva.commons.exceptions.PlayerDoesNotExistException;
+import de.hochschule.bremen.minerva.commons.exceptions.PlayerExistsException;
+import de.hochschule.bremen.minerva.commons.exceptions.WorldFileExtensionException;
+import de.hochschule.bremen.minerva.commons.exceptions.WorldFileNotFoundException;
+import de.hochschule.bremen.minerva.commons.exceptions.WorldFileParseException;
+import de.hochschule.bremen.minerva.commons.exceptions.WorldNotDefinedException;
+import de.hochschule.bremen.minerva.commons.exceptions.WorldNotStorable;
+import de.hochschule.bremen.minerva.commons.exceptions.WrongPasswordException;
+import de.hochschule.bremen.minerva.commons.vo.AttackResult;
+import de.hochschule.bremen.minerva.commons.vo.Country;
+import de.hochschule.bremen.minerva.commons.vo.CountryCard;
+import de.hochschule.bremen.minerva.commons.vo.Mission;
+import de.hochschule.bremen.minerva.commons.vo.Player;
+import de.hochschule.bremen.minerva.commons.vo.World;
+
+/**
+ * The game engine.
+ * A facade interface, that represents a lightweight subsystem access.
+ * 
+ * @since 1.0
+ * @version $Id$
+ * 
+ */
+public interface GameEngine {
+
+	// --------------------------------------
+	// -- login and registration subsystem --
+	// --------------------------------------
+	public void login(Player player) throws GameAlreadyStartedException, WrongPasswordException, PlayerDoesNotExistException, NoPlayerSlotAvailableException, DataAccessException;
+
+	public void register(Player player) throws PlayerExistsException, DataAccessException ;
+
+	// ---------------------
+	// -- world subsystem --
+	// ---------------------
+	public Vector<World> getWorldList() throws DataAccessException;
+
+	public Vector<World> getWorldList(boolean lite) throws DataAccessException ;
+
+	public void importWorld(File worldFile) throws WorldNotStorable, WorldFileNotFoundException, WorldFileExtensionException, WorldFileParseException, DataAccessException;
+
+	// ----------------------------
+	// -- game core logic subsystem
+	// ----------------------------
+	public Vector<Player> getPlayers();
+
+	public Vector<Mission> getMissions();
+
+	public void startGame() throws NotEnoughPlayersLoggedInException, NoPlayerLoggedInException, WorldNotDefinedException;
+
+	public void killGame(boolean createNewOne) throws DataAccessException ;
+
+	public void setGameWorld(World world);
+
+	public World getGameWorld();
+
+	public boolean isGameFinished();
+
+	public Player getGameWinner();
+
+	public void releaseCard(CountryCard card);
+
+	public void releaseCards(Vector<CountryCard> cards);
+	
+	public int getAllocatableArmyCount();
+
+	public void allocateArmy(Country allocatable) throws NotEnoughArmiesException, CountryOwnerException;
+
+	public AttackResult attack(Country source, Country destination, int armyCount) throws CountriesNotInRelationException, NotEnoughArmiesException, IsOwnCountryException;
+	
+	public void move(Country source, Country destination, int armyCount) throws CountriesNotInRelationException, NotEnoughArmiesException, CountryOwnerException;
+
+	public void finishTurn();
+}
---
+++ minerva-commons/src/de/hochschule/bremen/minerva/commons/util/MapTool.java
@@ -0,0 +1,84 @@
+/*
+ * Minerva - Game, Copyright 2010 Christian Bollmann, Carina Strempel, André König
+ * Hochschule Bremen - University of Applied Sciences - All Rights Reserved.
+ *
+ * $Id$
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Contact:
+ *     Christian Bollmann: cbollmann@stud.hs-bremen.de
+ *     Carina Strempel: cstrempel@stud.hs-bremen.de
+ *     André König: akoenig@stud.hs-bremen.de
+ * 
+ * Web:
+ *     http://minerva.idira.de
+ * 
+ */
+package de.hochschule.bremen.minerva.commons.util;
+
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.util.HashMap;
+
+import de.hochschule.bremen.minerva.commons.vo.Country;
+import de.hochschule.bremen.minerva.commons.vo.ValueObject;
+import de.hochschule.bremen.minerva.commons.vo.World;
+
+/**
+ * Provides some helper methods for map processing.
+ *
+ * @since 1.0
+ * @version $Id$
+ * 
+ */
+public class MapTool {
+
+	// The color on a map, that identifies the center point,
+	// where to place some controls (e. g. "army count").
+	private static final int COUNTRY_ANCHOR_IDENTIFIER = 0xFF000000;
+	
+	/**
+	 * Returns a hash map, which contains the country anchor points.
+	 * A country anchor means the center point where controls, the
+	 * army count for example, can be placed.
+	 * 
+	 * Checking each map pixel if the color equals the COUNTRY_ANCHOR_IDENTIFIER
+	 * color. If so, a new anchor was found and will be placed in the hash map
+	 * identified by the country.
+	 * 
+	 * @return HashMap Contains the country as key and the anchor point as value.
+	 * 
+	 */
+	public static HashMap<Country, Point> getCountryAnchors(BufferedImage map, BufferedImage mapUnderlay, World world) {
+		HashMap<Country, Point> countryAnchors = new HashMap<Country, Point>();
+
+		for (int x = 0; x < map.getWidth(); x++) {
+			for (int y = 0; y < map.getHeight(); y++) {
+				if (map.getRGB(x, y) == COUNTRY_ANCHOR_IDENTIFIER) {
+					Point point = new Point(x,y);
+					Country country = world.getCountry(new Color(mapUnderlay.getRGB(x, y)));
+
+					if (country.getId() > ValueObject.getDefaultId()) {
+						countryAnchors.put(country, point);
+					}
+				}
+			}
+		}
+		
+		return countryAnchors;
+	}
+}
